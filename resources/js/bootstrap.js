c4cd39e592959ed544abd9286e5c86f0ee929ee7
felexkemboi <felokemboi10@gmail.com>
2020-03-19 11:22:47 +0300

    Comment the content of bootstrap.js to test if it main component can work without

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,7 @@
 window._ = require('lodash');
 window.Popper = require('popper.js').default;
+window.axios = require('axios');
+
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -7,11 +9,13 @@ window.Popper = require('popper.js').default;
  * code may be modified to fit the specific needs of your application.
  */
 
+/*
 try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
 } catch (e) {}
+*/
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -19,9 +23,8 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
@@ -29,20 +32,21 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * a simple convenience so we don't have to attach every token manually.
  */
 
+/*
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
-
+} */
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
 
+/*
 import Echo from 'laravel-echo'
 
 window.Pusher = require('pusher-js');
@@ -53,3 +57,4 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     encrypted: false
 });
+*/
